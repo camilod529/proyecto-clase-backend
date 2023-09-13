80aa8012cbeee124b6c03776c18072c663b944c3
chokpikLaptop <camilod529@gmail.com>
2023-09-12 19:08:43 -0500

    clase 12-09-23, creacion inicial proyecto laravel

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('product', function () {
+    return "Estoy en la pagina de producto";
+});
+
+Route::get('product/create', function () {
+    return "Formulario de la creacion de archivos";
+});
+
+Route::get('product/{id}', function ($id) {
+    return "Estoy en la pagina del producto $id";
+});
+Route::get('product/{id}/{category}', function ($id, $category) {
+    return "Estoy en la pagina del producto $id en la categoria $category";
+});
